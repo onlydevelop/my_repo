7581461738c0e125dd3f00c2f91f1da09bacce1f
Dipanjan Bhowmik <onlydevelop@gmail.com>
2020-01-10 07:00:32 +0530

    Added a resource leak

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,11 +1,24 @@
 package com.example;
 
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
 
     public int subtract(int a, int b) {
+        try {
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            // myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
         return a - b;
     }
 }
